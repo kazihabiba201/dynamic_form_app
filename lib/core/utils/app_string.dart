4c4758128898518f7b927a9d1477452c26f4864a
kazihabiba201 <kazihabiba125@gmail.com>
2025-08-02 00:23:16 +0600

    Added missing strings in AppStrings

--- lib/core/utils/app_string.dart
+++ lib/core/utils/app_string.dart
@@ -3,12 +3,22 @@ class AppStrings {
 
   static const String formListSubTitle = 'Tap to start filling this form';
   static const String pickImage = 'Pick Image';
+  static const String savePDF = 'Save as PDF';
+  static const String saveTXT = 'Save as TXT';
   static const String submitBtn = 'Submit';
   static const String leastNumberWarning = 'must be at least';
   static const String lessNumberWarning = 'must be less than';
   static const String characters = 'characters.';
+
+  //Toast
+  static const String showSuccessSnackBar = 'TXT saved successfully at:';
+  static const String showErrorSnackBar = 'Error saving JSON file';
+  static const String showImageError = 'Image not found at';
+  static const String showSavePDFToast = 'PDF saved at:';
+  static const String showErrorSavePDFToast = 'Error saving/opening PDF:';
   static const String pleaseCompleteAllFieldsProperlySnackBarText = 'Please complete all fields properly.';
 
   // AppBarsTitle
   static const String formListTitle = 'Dynamic Form List';
+  static const String submissionPreviewTitle = 'Submission Preview';
 }
