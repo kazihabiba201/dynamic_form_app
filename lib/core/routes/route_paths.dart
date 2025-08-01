7c56fd6c304c8dd3eeef2d9f80d25db32188023f
kazihabiba201 <kazihabiba125@gmail.com>
2025-08-01 22:57:40 +0600

    Added Submission View Page route.

--- lib/core/routes/route_paths.dart
+++ lib/core/routes/route_paths.dart
@@ -1,13 +1,14 @@
 import 'package:dynamic_form_app/core/routes/routes.dart';
 import 'package:dynamic_form_app/presentation/screens/form_list_screen.dart';
 import 'package:dynamic_form_app/presentation/screens/form_screen.dart';
+import 'package:dynamic_form_app/presentation/screens/submission_view_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: RoutePaths.formList,
   routes: [
     GoRoute(path: RoutePaths.formList, name: RouteName.formList, builder: (context, state) => const FormListScreen()),
-
     GoRoute(path: RoutePaths.fORM, name: RouteName.fORM, builder: (context, state) => const FormScreen()),
+    GoRoute(path: RoutePaths.formSubmission, name: RouteName.formSubmission, builder: (context, state) => const SubmissionViewPage()),
   ],
 );
