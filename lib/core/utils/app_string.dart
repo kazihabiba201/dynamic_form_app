461b2153c2fe8d3f592d710279620af6fee88f97
kazihabiba201 <kazihabiba125@gmail.com>
2025-08-01 22:55:54 +0600

    Added localized strings for field labels and validation

--- lib/core/utils/app_string.dart
+++ lib/core/utils/app_string.dart
@@ -2,6 +2,12 @@ class AppStrings {
   static const String appName = 'Dynamic Form App';
 
   static const String formListSubTitle = 'Tap to start filling this form';
+  static const String pickImage = 'Pick Image';
+  static const String submitBtn = 'Submit';
+  static const String leastNumberWarning = 'must be at least';
+  static const String lessNumberWarning = 'must be less than';
+  static const String characters = 'characters.';
+  static const String pleaseCompleteAllFieldsProperlySnackBarText = 'Please complete all fields properly.';
 
   // AppBarsTitle
   static const String formListTitle = 'Dynamic Form List';
